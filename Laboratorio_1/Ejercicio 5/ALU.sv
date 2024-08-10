964e4c9324637b945a3f63efe9c3813aa639f8f5
Brayan <bbarqueromadrigal@gmail.com>
2024-08-09 21:06:59 -0600

    ALU completada, pendiente el testbench para la verificación del funcionamiento

--- Laboratorio_1/Ejercicio 5/ALU.sv
+++ Laboratorio_1/Ejercicio 5/ALU.sv
@@ -4,74 +4,84 @@ module ALU #(parameter n = 4) (
     input logic ALUFlagIn,
     input logic [3:0] ALUControl,
     output logic [n-1:0] ALUResult,
-    output logic [1:0] ALUFlags //bit 0 para bandera "C" y bit 1 para bandera "Z" (Cero)
+    output logic [1:0] ALUFlags //bit 0 para bandera "C" (bit de salida del corrimiento) y bit 1 para bandera "Z" (resultado cero)
 );
 
 typedef enum logic[3:0] {op_and, op_or, suma, incremento_1, decremento_1, op_not, resta, op_xor, corrimiento_izq, corrimiento_der} operations;
 
 
-logic [n:0] shift_result;
+logic [n-1:0] fill_pattern;
+logic [n+1:0] shift_out;
 
     always_comb begin
+        
+        fill_pattern = ALUFlagIn ? {n{1'b1}} : {n{1'b0}};
+        
         case (ALUControl)
-            op_and: 
+            op_and: begin
                 ALUResult = ALUA & ALUB;
-           
-            op_or: 
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
+            end
+            
+            op_or: begin
                 ALUResult = ALUA | ALUB;
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
+            end
             
             suma: begin
                 ALUResult = ALUA + ALUB + ALUFlagIn;
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
             end
            
-            incremento_1: 
-                if (ALUFlagIn)
-                    ALUResult = ALUB + 1;
-                else
-                    ALUResult = ALUA + 1;
+            incremento_1: begin
+                ALUResult = ALUFlagIn ? (ALUB + 1) : (ALUA + 1);
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
+            end
 
-            decremento_1: 
-                if (ALUFlagIn)
-                    ALUResult = ALUB - 1;
-                else
-                    ALUResult = ALUA - 1;
+            decremento_1: begin
+                ALUResult = ALUFlagIn ? (ALUB - 1) : (ALUA - 1);
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
+            end
             
             op_not: begin
-                if (ALUFlagIn)
-                    ALUResult = ~ALUB;
-                else
-                    ALUResult = ~ALUA;
+                ALUResult = ALUFlagIn ? ~ALUB : ~ALUA;
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
             end
             
             resta: begin
                 ALUResult = ALUA - ALUB - ALUFlagIn;
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
             end
             
-            op_xor: 
+            op_xor: begin
                 ALUResult = ALUA ^ ALUB;
-                
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
+            end
+            
             corrimiento_izq: begin
-                shift_result = ALUA << ALUB;
-                ALUResult = shift_result[n-1:0];
-                if (shift_result[n])
-                    ALUFlags=2'b01;
+                shift_out [n:1] = ALUA << ALUB;
+                ALUFlags[0] = shift_out[n+1];
+                if (ALUB >= n)
+                    ALUResult = fill_pattern;
                 else
-                    ALUFlags=2'b00;
+                    ALUResult = (ALUA << ALUB) | (fill_pattern >> (n - ALUB));
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
             end            
             
             corrimiento_der: begin
-                shift_result = ALUA >> ALUB;
-                ALUResult = shift_result[n-1:0];
-                if (shift_result[n])
-                    ALUFlags=2'b01;
+                shift_out [n:1] = ALUA >> ALUB;
+                ALUFlags[0] = shift_out[0];
+                if (ALUB >= n)
+                    ALUResult = fill_pattern;
                 else
-                    ALUFlags=2'b00;
+                    ALUResult = (ALUA >> ALUB) | (fill_pattern << (n - ALUB));
+                ALUFlags[1] = (ALUResult == 0) ? 1 : 0;
             end 
                 
            
             default: begin
                 ALUResult = 0;
-                ALUFlags = 0;
+                ALUFlags = 2'b10;
             end
         endcase
     end
