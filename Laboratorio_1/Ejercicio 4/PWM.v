9d1a8cdcc347bbb895c3cd5ed3f7320f5d611015
Jeffrey823 <jeffrey080203@gmail.com>
2024-08-12 20:55:45 -0600

    Corrección al codigo PWM

--- Laboratorio_1/Ejercicio 4/PWM.v
+++ Laboratorio_1/Ejercicio 4/PWM.v
@@ -1,7 +1,7 @@
 
-module PWM (
+module PWM(
     input wire clk //Clock
-    input rst_n //Reset Input
+    input wire rst //Reset Input
 
     input wire [3:0] c_trabajo, //Input de 4 bits
     
@@ -12,15 +12,22 @@ module PWM (
     reg [14:0] contador; // Contador de 15 bits
     reg [14:0] corte; // Valor de corte basado en ciclos de trabajo
 
-    //1ms es 27000 ciclos; 27MHz = 27000000
+    //1ms es 27000 ciclos; 27MHz = 27000000 ciclos
     parameter PERIODO = 27000;
 
     always @(posedge clk) begin
-        contador <= contador + 1;
-        if (contador >= PERIODO - 1) // Si el contador llega al periodo (27000) se resetea
-            contador <= 0;
-        corte <= (c_trabajo * (PERIODO / 16)); // Se actualiza el valor de corte segun el ciclo de trabajo
-        pwm_out <= (contador < corte) ? 1 : 0; // Se genera la salida PWM
+        if (rst) begin
+            counter <= 0;
+            pwm_out <= 0; 
+        end
 
+        else begin 
+            contador <= contador + 1;
+            if (contador >= PERIODO - 1) begin // Si el contador llega al periodo (27000) se resetea
+                contador <= 0;
+            end
+            corte <= (c_trabajo * (PERIODO / 15)); // Se actualiza el valor de corte segun el ciclo de trabajo
+            pwm_out <= (contador < corte) ? 1 : 0; // Se genera la salida PWM
+        end
     end
 endmodule
