4114f7d65a786afae8aa80e49a667b757367aee6
brayan832015 <bbarqueromadrigal@gmail.com>
2024-08-16 15:56:09 -0600

    Ajustes constraints y código del módulo (LEDs encienden con 0)

--- Laboratorio_1/Ejercicio 2/src/LEDs.v
+++ Laboratorio_1/Ejercicio 2/src/LEDs.v
@@ -3,7 +3,9 @@ module switch_to_led (
     output [3:0] leds
 );
 
+    wire [3:0] compl_2;
     // Calcular el complemento a 2
-    assign leds = ~switches + 4'b0001;
+    assign compl_2 = ~switches + 4'b0001;
+    assign leds = ~compl_2; //leds encienden con 0
 
 endmodule
